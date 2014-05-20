ce42bba1f3b6a6bf6b0012d1f4636112624e0bbd
Carlos <cm.rios.10@gmail.com>
2014-05-20 00:46:56 -0300

    add new functionalitites and organize the files

--- functions.R
+++ functions.R
@@ -6,3 +6,20 @@ editingLabel <- function(x){
     gsub("-|,","_",x)
     
 }
+#
+#
+func<-function(x) {
+        la<-lapply(x[1:66],mean)
+        data.frame(la)
+}
+#
+#
+getDescriptiveActivity <- function (activitis) {
+        activitis[which(activitis=="1"),]<-"WALKING"
+        activitis[which(activitis=="2"),]<-"WALKING_UPSTAIRS"
+        activitis[which(activitis=="3"),]<-"WALKING_DOWNSTAIRS"
+        activitis[which(activitis=="4"),]<-"SITTING"
+        activitis[which(activitis=="5"),]<-"STANDING"
+        activitis[which(activitis=="6"),]<-"LAYING"
+        return(activitis)
+}
--- run_analysis.R
+++ run_analysis.R
@@ -1,34 +1,48 @@
-#1-Merges the training and the test sets to create one data set.
-setwd("C:/Users/Usuarioç/Desktop/carlos/Getting Cleaning Data/GettingCleaningDataProject")
+setwd("D:/coursera-gettingandcleaningdata/GettingCleaningDataProject")
 getwd()
-
+rm(list=ls())
+library("plyr")
 source("functions.R")
+
+#load files
 trainSetFile<-"UCI HAR Dataset/train/X_train.txt"
 trainLabelsFile<-"UCI HAR Dataset/train/y_train.txt"
 trainSubjectFile<-"UCI HAR Dataset/train/subject_train.txt"
-
-trainSet<-read.table(trainSetFile,colClasses="numeric")
-trainLabels<-read.table(trainLabelsFile,colClasses="numeric")
-trainSubject<-read.table(trainSubjectFile,colClasses="numeric")
-
 testSetFile<-"UCI HAR Dataset/test/X_test.txt"
 testLabelsFile<-"UCI HAR Dataset/test/y_test.txt"
 testSubjectFile<-"UCI HAR Dataset/test/subject_test.txt"
+featuresFile<-"UCI HAR Dataset/features.txt"
 
-
+features<-read.table(featuresFile,colClasses="character")
+trainSet<-read.table(trainSetFile,colClasses="numeric")
+trainLabels<-read.table(trainLabelsFile,colClasses="numeric")
+trainSubject<-read.table(trainSubjectFile,colClasses="numeric")
 testSet<-read.table(testSetFile,colClasses="numeric")
 testLabels<-read.table(testLabelsFile,colClasses="numeric")
 testSubject<-read.table(testSubjectFile,colClasses="numeric")
 
-mergeDataSet<-rbind(train,test)
-featuresFile<-"UCI HAR Dataset/features.txt"
-features<-read.table(featuresFile,colClasses="character")
-features<-sapply(as.list(features[[2]]),editingLabel)
-names(mergeDataSet)<-features
-dd<-cbind(mergeDataSet[,grepl("mean()",features[[2]],fixed=T)],mergeDataSet[,grepl("std()",features[[2]],fixed=T)])
-activities =rbind(trainLabels,testLabels)
-subjects = rbind(trainSubject,testSubject)
-dd<-cbind(dd,activities=activities[[1]],subjects=subjects[[1]])
+#1-Merges the training and the test sets to create one data set.
+mergeDataSet<-rbind(trainSet,testSet)
 
+#Extracts only the measurements on the mean and standard deviation for each measurement. 
+mergeDataSetMeanStd<-cbind(mergeDataSet[,grepl("mean()",features[[2]],fixed=T)],mergeDataSet[,grepl("std()",features[[2]],fixed=T)])
 
+#Uses descriptive activity names to name the activities in the data set
+activitis<-rbind(trainLabels,testLabels)
+activitis<-getDescriptiveActivity(activitis)
+subject<-rbind(trainSubject,testSubject)
+mergeDataSetMeanStd<-cbind(mergeDataSetMeanStd,activitis)
+mergeDataSetMeanStd<-cbind(mergeDataSetMeanStd,subject)
 
+#Appropriately labels the data set with descriptive activity names. 
+featuresMeanStd<-rbind(features[grepl("mean()",features[[2]],fixed=T),],features[grepl("std()",features[[2]],fixed=T),])
+featuresMeanStd<-sapply(as.list(featuresMeanStd[[2]]),editingLabel)
+names(mergeDataSetMeanStd)<-c(featuresMeanStd,"activity","subject")
+
+#Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+# write.csv(mergeDataSetMeanStd,"mergeDataSetMeanStd.csv",row.names =FALSE)
+# mergeDataSetMeanStd<-read.csv("mergeDataSetMeanStd.csv")
+finalTidyDataSet<-ddply(mergeDataSetMeanStd,.(activity,subject),func)
+write.table(finalTidyDataSet, "finalTidyDataSet.txt", sep="\t",row.names =FALSE)
+tidy<-read.table("finalTidyDataSet.txt",header=T)
+View(tidy)
